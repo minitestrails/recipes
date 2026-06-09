ac3a24928b1a7a0542b8bbf15e04a3f24e1d5732
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2026-06-09 12:27:54 +0545

    Require Recipe title

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "rejects a blank title" do
+    recipe = Recipe.new(title: "")
+    assert_not recipe.valid?
+    assert_includes recipe.errors[:title], "can't be blank"
+  end
 end

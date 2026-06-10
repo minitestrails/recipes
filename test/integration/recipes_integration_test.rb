21b00bfeb5bfd2f14f619916acfa264645060722
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2026-06-10 13:57:31 +0545

    Add Recipes integration tests

---
+++ test/integration/recipes_integration_test.rb
@@ -0,0 +1,22 @@
+# test/integration/recipes_integration_test.rb
+require "test_helper"
+
+class RecipesIntegrationTest < ActionDispatch::IntegrationTest
+  test "lists recipes" do
+    get recipes_url
+    assert_response :success
+  end
+
+  test "creates a recipe" do
+    get new_recipe_url
+    assert_response :success
+
+    assert_difference("Recipe.count", 1) do
+      post recipes_url, params: { recipe: { title: "Lentil soup" } }
+    end
+
+    assert_redirected_to recipe_url(Recipe.last)
+    follow_redirect!
+    assert_response :success
+  end
+end

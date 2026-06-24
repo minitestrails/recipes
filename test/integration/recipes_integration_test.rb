7de2a3f0384f6ae76583e27776a1e7f14c0bc73d
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2026-06-24 14:29:49 +0545

    Add destroy confirmation and Recipes CRUD system tests

--- test/integration/recipes_integration_test.rb
+++ test/integration/recipes_integration_test.rb
@@ -2,10 +2,11 @@
 require "test_helper"
 
 class RecipesIntegrationTest < ActionDispatch::IntegrationTest
-  test "lists recipes" do
+  test "visits the list" do
     get recipes_url
     assert_response :success
     assert_match recipes(:pancakes).title, response.body
+    assert_select "#recipes div[id^='recipe_']", count: Recipe.count
   end
 
   test "creates a recipe" do
@@ -24,5 +25,30 @@ class RecipesIntegrationTest < ActionDispatch::IntegrationTest
   test "shows a recipe" do
     get recipe_url(recipes(:pancakes))
     assert_response :success
+  end
+
+  test "updates a recipe" do
+    recipe = recipes(:pancakes)
+
+    get edit_recipe_url(recipe)
+    assert_response :success
+
+    patch recipe_url(recipe),
+          params: {
+            recipe: {
+              title: "Extra fluffy pancakes"
+            }
+          }
+    assert_redirected_to recipe_url(recipe)
+    follow_redirect!
+    assert_response :success
+    assert_equal "Extra fluffy pancakes", recipe.reload.title
+  end
+
+  test "destroys a recipe" do
+    recipe = recipes(:lentil_soup)
+
+    assert_difference("Recipe.count", -1) { delete recipe_url(recipe) }
+    assert_redirected_to recipes_url
   end
 end
